2a2e5cd63f265cd62a3d09e5ffc53d8b393ac96d
eddie <39470805+eddiek123@users.noreply.github.com>
2018-10-23 20:20:38 -0700

    first commit

---
+++ data/table.js
@@ -0,0 +1,10 @@
+const tableArray = [
+  {
+    name: "Eddie",
+    email: "eddie.kader@gmail.com",
+    ID: "eddiek123",
+    Phone: 555-555-555;
+  }
+]
+
+module.exports = tableArray;
---
+++ data/waitlist.js
@@ -0,0 +1,9 @@
+const waitlistArray = [{
+  name: "Sam",
+  email: "Sam.samawi@gmail.com",
+  ID: "sammy123",
+  Phone: 555 - 555 - 555;
+}]
+
+
+ module.exports = waitlistArray;
